d1951f6eccb551c8592df14a23d3b66dc303504b
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-05-02 01:29:55 +0300

    feat: init the project structure with required packages

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen w-screen bg-background text-white flex justify-center items-center">
+      <h1 className="text-5xl font-bold">Quote Tab</h1>
+    </div>
+  );
+}
